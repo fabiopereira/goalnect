c66380e99aa2c530db95829776d7ac51389ec39b
Goalnect <goalnect@Goalnects-MacBook-Pro.local>
2012-09-20 09:39:48 -0300

    staging settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -52,8 +52,6 @@ Goalnect::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   
   # http://stackoverflow.com/questions/8186584/how-do-i-set-up-email-confirmation-with-devise
-  
-  
   config.action_mailer.default_url_options = { :host => 'staging-goalnect.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
