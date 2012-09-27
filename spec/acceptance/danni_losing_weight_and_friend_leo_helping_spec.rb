912648c30bd8e7982a8093d8abfdae45ca1940e4
Fabio Pereira <fabiopereira.me@gmail.com>
2012-09-27 14:54:42 -0300

    Testing using steak

--- spec/acceptance/danni_losing_weight_and_friend_leo_helping_spec.rb
+++ spec/acceptance/danni_losing_weight_and_friend_leo_helping_spec.rb
@@ -6,10 +6,45 @@ feature 'Danni losing weight and friend leo helping', %q{
 } do
 
   scenario 'Danni 12 months journey losing 35kg' do
-    danni = FactoryGirl.build(:any_user)
+    # danni = FactoryGirl.attributes_for(:any_user)
+    
+    danni = {
+      'email' => 'danni@goalnect.com',
+      'password' => '123456',
+      'screen_name' => 'Danni',
+      'username' => 'danni'
+    }
+    
+    # puts 'Dannis email - ' + danni['email']
+    
     visit '/'
     click_on 'Login'
-    page.should have_content 'Sign in'
+    page.should have_content 'Sign in'            
+    
+    
+    fill_in "user_email",    :with => danni['email']
+    fill_in 'user_password', :with => danni['password']
+    click_on 'Sign in'
+    page.should have_content 'Invalid email or password'
+    
+    click_on 'Sign up'
+    page.should have_content 'Sign up'
+    
+    click_on 'Sign up'
+    page.should have_content 'errors'
+    
+    fill_in 'user_email', :with => danni['email']
+    fill_in 'user_password', :with => danni['password']
+    fill_in 'user_password_confirmation', :with => danni['password']
+    fill_in 'user_screen_name', :with => danni['screen_name'] 
+    fill_in 'user_username', :with => danni['username'] 
+    # fill_in 'user_dob', :with => danni['dob'].strftime("%d/%m/%Y") 
+    fill_in 'dob', :with => 27.years.ago.strftime("%d/%m/%Y") 
+    
+    # click_on 'Sign up'
+    # 
+    # page.should have_content 'confirmation link has been sent to your email'
+    
   end
 
 end
