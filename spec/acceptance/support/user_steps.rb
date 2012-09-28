251aefc53bb063dcf77f9caae9b5af5739cf2390
Fabio Pereira <fabiopereira.me@gmail.com>
2012-09-28 11:55:58 -0300

    Refactoring user steps

--- spec/acceptance/support/user_steps.rb
+++ spec/acceptance/support/user_steps.rb
@@ -1,9 +1,43 @@
 module UserSteps
   
-  PASSWORD = '123456'
+  PASSWORD = '123456'  
+  
+  def logout_current_user
+    if page.has_css?('#current_user_username')
+      click_on 'Logout'
+    end
+  end
   
   def is_logged_in username
     page.has_css?('#current_user_username') && page.find('#current_user_username').text == username
+  end        
+  
+  def login_user user
+    visit '/'   
+    click_on 'Sign in'   
+    fill_in 'user_email', :with => user.email
+    fill_in 'user_password', :with => PASSWORD
+    click_on 'Sign in'   
+    page.should have_content 'ABOUT ME'       
+  end          
+  
+  def sign_up username
+    user = FactoryGirl.build(:any_user, :username => username)   
+    visit '/'   
+    click_on 'Login'
+    click_on 'Sign up'
+    page.should have_content 'Sign up' 
+    fill_in 'user_email', :with => user.email
+    fill_in 'user_password', :with => '123456'
+    fill_in 'user_password_confirmation', :with => '123456'
+    fill_in 'user_screen_name', :with => user.screen_name
+    fill_in 'user_username', :with => user.username 
+    fill_in 'dob', :with => user.dob.strftime("%d/%m/%Y")
+    click_on 'Sign up'   
+    user = User.find_by_username(user.username)
+    visit "/users/confirmation?confirmation_token=#{user.confirmation_token}" 
+    page.should have_content 'ABOUT ME'       
+    user = User.find_by_username(user.username)
   end
   
 	def logged_in username          
@@ -14,34 +48,12 @@ module UserSteps
 	    return user
     end
 
-    if page.has_css?('#current_user_username')
-      click_on 'Logout'
-    end
+    logout_current_user
 
 	  if !user
-      user = FactoryGirl.build(:any_user, :username => username)   
-      visit '/'   
-      click_on 'Login'
-      click_on 'Sign up'
-      page.should have_content 'Sign up' 
-      fill_in 'user_email', :with => user.email
-      fill_in 'user_password', :with => '123456'
-      fill_in 'user_password_confirmation', :with => '123456'
-      fill_in 'user_screen_name', :with => user.screen_name
-      fill_in 'user_username', :with => user.username 
-      fill_in 'dob', :with => user.dob.strftime("%d/%m/%Y")
-      click_on 'Sign up'   
-      user = User.find_by_username(user.username)
-      visit "/users/confirmation?confirmation_token=#{user.confirmation_token}" 
-      page.should have_content 'ABOUT ME'       
-      user = User.find_by_username(user.username)
+	    user = sign_up username
     else 
-      visit '/'   
-      click_on 'Sign in'   
-      fill_in 'user_email', :with => user.email
-      fill_in 'user_password', :with => PASSWORD
-      click_on 'Sign in'   
-      page.should have_content 'ABOUT ME'       
+      login_user user
 	  end
 	  
 	  user
