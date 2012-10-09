9cdd17c2f542b91d7b86493a264c24bdce917fd4
Fabio Pereira <fabiopereira.me@gmail.com>
2012-10-09 20:33:32 -0300

    Adding aliases

---
+++ doc/aliases.sh
@@ -0,0 +1,18 @@
+# Add this to your ~/.bash_profile or equivalent
+
+# Git Aliases
+alias gs='git status'
+alias ga='git add'
+alias gci='git commit -am'
+alias gco='git checkout '
+alias gp='git push'
+alias gpu='git pull -u'
+alias gpo='git push origin'
+alias gpom='git push origin master'
+alias gphm='git push heroku master'
+alias gdm='git diff | mate'
+alias gsci='git show --pretty="format:" --name-only'
+alias gclean="git clean -f -d"
+alias gh="cat ~/.bash_profile | grep 'alias g'"
+
+alias bi="bundle install"
