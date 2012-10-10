70fe88b4d0dfd9160745cea48fdc69abd312d97d
Camila Hayashi <camilahayashi@gmail.com>
2012-10-10 18:50:16 -0300

    allow people to see goalnecters/goals/donate without login in

--- app/controllers/achiever_controller.rb
+++ app/controllers/achiever_controller.rb
@@ -1,5 +1,5 @@
 class AchieverController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :except => [:view, :search] 
   
   def view
     user_username = params[:user_username]
