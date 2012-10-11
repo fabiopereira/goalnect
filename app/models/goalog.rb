1bf257894ddb49f1893a9ddf1cf2d3bdf6b22844
Fabio Pereira <fabiopereira.me@gmail.com>
2012-10-11 20:16:27 -0300

    Using Goalog

---
+++ app/models/goalog.rb
@@ -0,0 +1,24 @@
+class Goalog
+  
+  def self.error m
+    Rails.logger.error "[goalog:error] #{m}"
+  end
+  
+  def self.critical m
+    Rails.logger.error "[goalog:critical] #{m}"
+  end
+  
+  def self.info m
+    Rails.logger.info "[goalog:info] #{m}"
+  end
+  
+  def self.debug m
+    Rails.logger.debug "[goalog:debug] #{m}"
+  end
+  
+  def self.exception e
+    Rails.logger.info "[goalog:exception] #{e.message}"
+    Rails.logger.info "[goalog:exception] #{e.backtrace.join("\n")}"
+  end
+  
+end
