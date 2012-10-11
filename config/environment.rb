95de90d2dc77946d0170f9cfd491fc2927598ba5
Fabio Pereira <fabiopereira.me@gmail.com>
2012-10-11 17:05:34 -0300

    testing rails.logger and le

--- config/environment.rb
+++ config/environment.rb
@@ -7,3 +7,7 @@ Goalnect::Application.initialize!
 #ENV['RAILS_ENV'] ||= 'production'
 
 Date::DATE_FORMATS.merge!(:default => "%d/%m/%Y")
+
+if Rails.env.staging?
+	Rails.logger = Le.new(ENV["letoken"])
+end
