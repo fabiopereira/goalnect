9b61d9d770c4af5471024c15448b95155f9227f4
Camila Hayashi <camilahayashi@gmail.com>
2012-10-12 04:36:45 -0300

    i18n charities registrations

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -1,3 +1,6 @@
 class Charity < ActiveRecord::Base
   attr_accessible :charity_name, :contact_name, :description, :email, :phone
+  
+  validates_presence_of :charity_name, :contact_name, :email, :phone
+  
 end
