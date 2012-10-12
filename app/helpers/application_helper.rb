9fc47843621f7b50f02e09a5eee8bf4a7f51c0be
Camila Hayashi <camilahayashi@gmail.com>
2012-10-11 21:00:31 -0300

    add facebook social plugins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     '/users/sign_in?user_return_to=' + request.path
   end
   
+  def current_url
+    if Rails.env.development? or Rails.env.test?
+      'http://' + request.host + ':' + request.port.to_s + request.fullpath
+    else
+      'http://' + request.host + request.fullpath
+    end
+  end
+  
 end
