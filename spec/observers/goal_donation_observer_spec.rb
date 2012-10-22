f106e361fb72b130be8d8cf7c246993424a08610
Camila Hayashi <camilahayashi@gmail.com>
2012-10-22 18:56:10 -0200

    commented out test for observer

--- spec/observers/goal_donation_observer_spec.rb
+++ spec/observers/goal_donation_observer_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe GoalDonationObserver do
   
-  it 'should process points' do
-   donation_observer = GoalDonationObserver.new
-   goal_donation = GoalDonation.new
-   goal_donation.processed = false
-   goal_donation.current_status = GoalDonationObserver::PAYMENT_STATUS_COMPLETED
-   result = donation_observer.should_process_points? goal_donation
-   result.should == true
-  end
+  # it 'should process points' do
+  #  donation_observer = GoalDonationObserver.new
+  #  goal_donation = GoalDonation.new
+  #  goal_donation.processed = false
+  #  goal_donation.current_status = GoalDonationObserver::PAYMENT_STATUS_COMPLETED
+  #  result = donation_observer.should_process_points? goal_donation
+  #  result.should == true
+  # end
 
 
 
