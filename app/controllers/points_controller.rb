a8ba2735ca3b2785eb699f826fb0773cf8c2e9af
Fabio Pereira <fabiopereira.me@gmail.com>
2012-10-23 16:39:58 -0200

    implementing point statement

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -2,7 +2,7 @@ class PointsController < ApplicationController
   
   def points_statement
     @user = User.find_by_username(params[:user_username])
-    @point_transactions = GoalDonationPointTransaction.find_by_user_id(@user.id)
+    @point_transactions = GoalDonationPointTransaction.find_all_by_user_id(@user.id)
     respond_to do |format|
       format.html # points_statement.html.erb
       format.json { render json: @point_transactions }
--- app/models/goal_donation_point_transaction.rb
+++ app/models/goal_donation_point_transaction.rb
@@ -1,6 +1,9 @@
 class GoalDonationPointTransaction < ActiveRecord::Base
   attr_accessible :goal_donation_id, :goal_id, :point_amount, :user_id, :active
+  attr_accessible :user, :goal
   
-  belongs_to :user, :goal
-                                  
+  belongs_to :user
+  belongs_to :goal
+  belongs_to :goal_donation
+  
 end
