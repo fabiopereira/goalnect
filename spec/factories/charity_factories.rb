21be3b886868c0eefd9a827adbf9b51fe7f79026
Camila Hayashi <camilahayashi@gmail.com>
2012-10-26 13:35:15 -0200

    populate nickname

--- spec/factories/charity_factories.rb
+++ spec/factories/charity_factories.rb
@@ -3,6 +3,7 @@ require 'factory_girl_rails'
 FactoryGirl.define do
   factory :active_charity, class: Charity do
     sequence(:charity_name) {|n| "Charity #{n}"}
+    sequence(:nickname) {|n| "Charity#{n}"}
     contact_name {|c| "Contact #{c.charity_name}"}
     sequence(:email) {|n| "charity#{n}@mycharitydomain.com"}
     phone "2323424234"
