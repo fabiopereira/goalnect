7ae3c5cd3080b949300a709967e5f3e85afe1d9d
Camila Hayashi <camilahayashi@gmail.com>
2012-10-27 13:20:35 -0200

    added alignment to charity image

--- app/helpers/charities_helper.rb
+++ app/helpers/charities_helper.rb
@@ -1,9 +1,19 @@
 module CharitiesHelper
   
   def charity_image charity, image_type
+   
+    image_tag(define_image_to_use(charity, image_type))
+  end
+  
+  def charity_image_align charity, image_type, align
     charity_has_image = charity.image? && charity.image_url(image_type)
     image_url = charity_has_image ? charity.image_url(image_type) : "charity-nopic-#{image_type}.jpg" 
-    image_tag image_url
+    image_tag(define_image_to_use(charity, image_type), :align => align)
+  end
+  
+  def define_image_to_use charity, image_type
+     charity_has_image = charity.image? && charity.image_url(image_type)
+     image_url = charity_has_image ? charity.image_url(image_type) : "charity-nopic-#{image_type}.jpg"
   end
   
 end
