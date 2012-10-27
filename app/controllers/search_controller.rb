4126b7ac57d23bbf7a14c44a472f71069b77c440
Fabio Pereira <fabiopereira.me@gmail.com>
2012-10-27 11:13:39 -0200

    limiting search to 3 letters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,16 @@
 class SearchController < ApplicationController
+  MIN_SEARCH_QUERY_LENGTH = 3
 
   def search
+    @achievers = []
+    @charities = []
     q = params[:q]
-    @achievers = User.search(q)
-    @charities = Charity.search(q)
+    if q.length > MIN_SEARCH_QUERY_LENGTH
+      @achievers = User.search(q)
+      @charities = Charity.search(q)
+    else
+      flash[:error] = t "errors.search.length", :min => MIN_SEARCH_QUERY_LENGTH
+    end
   end
   
 end
