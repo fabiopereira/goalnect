30e73bff214ca916190b32a05e8fc0c1ad13d36a
Fabio Pereira <fabiopereira.me@gmail.com>
2012-10-30 19:20:28 -0200

    using pagseguro_token from goal_donation.goal.charity

--- app/controllers/goal_donation_payment_notifications_controller.rb
+++ app/controllers/goal_donation_payment_notifications_controller.rb
@@ -2,27 +2,38 @@ class GoalDonationPaymentNotificationsController < ApplicationController
   skip_before_filter :verify_authenticity_token
              
   def handle_notification notification
-    begin                                           
       goalnect_pagseguro_notification = GoalnectPagseguroNotification.new(notification)
       goalnect_pagseguro_notification.save_goal_donation_payment_notification
-    rescue Exception => e
-      Goalog.critical_exception "Error processing PagSeguro Notification #{YAML::dump(@notification)}", e
-    end
   end
   
   def confirm
     if request.post?
-      token = PagSeguro.config["authenticity_token"]
-      pagseguro_notification(token) do |notification|
-        handle_notification notification
+      begin                                           
+
+        @goal_donation = GoalDonation.find(params[:Referencia])
+        if @goal_donation.nil?
+          raise "Could not find GoalDonation for params[:Referencia] #{params[:Referencia]}"
+        end
+        token = @goal_donation.goal.charity.pagseguro_authenticity_token
+        
+        pagseguro_notification(token) do |notification|
+          handle_notification notification
+        end                    
+        
+        render :nothing => true
+        
+      rescue Exception => e
+        Goalog.critical_exception "Error processing PagSeguro Notification #{YAML::dump(@notification)}", e
       end
-      render :nothing => true
+        
     else
       Goalog.debug "REDIRECT FROM PagSeguro: GET"
       respond_to do |format|
         format.html { redirect_to root_path, notice: 'Donation received, waiting for pagseguro to confirm.' }
       end
     end
+    
+    
   end
   
 end
