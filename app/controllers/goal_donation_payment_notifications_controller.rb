0c95da540f4c654bb25dfbda37a3e57bdb358009
Fabio Pereira <fabiopereira.me@gmail.com>
2012-10-31 14:07:10 -0200

    fixing error handling for notification handling

--- app/controllers/goal_donation_payment_notifications_controller.rb
+++ app/controllers/goal_donation_payment_notifications_controller.rb
@@ -2,30 +2,29 @@ class GoalDonationPaymentNotificationsController < ApplicationController
   skip_before_filter :verify_authenticity_token
              
   def handle_notification notification
+    begin
       goalnect_pagseguro_notification = GoalnectPagseguroNotification.new(notification)
       goalnect_pagseguro_notification.save_goal_donation_payment_notification
+    rescue Exception => e
+      Goalog.critical_exception "Error processing PagSeguro Notification #{YAML::dump(@notification)}", e
+    end
+  end
+  
+  def get_token
+    @goal_donation = GoalDonation.find(params[:Referencia])
+    if @goal_donation.nil?
+      Goalog.critical "Could not find GoalDonation for params[:Referencia] #{params[:Referencia]}"
+    end
+    @goal_donation.goal.charity.pagseguro_authenticity_token
   end
   
   def confirm
     if request.post?
-      begin                                           
-
-        @goal_donation = GoalDonation.find(params[:Referencia])
-        if @goal_donation.nil?
-          raise "Could not find GoalDonation for params[:Referencia] #{params[:Referencia]}"
-        end
-        token = @goal_donation.goal.charity.pagseguro_authenticity_token
-        
-        pagseguro_notification(token) do |notification|
+        pagseguro_notification(get_token) do |notification|
           handle_notification notification
         end                    
         
         render :nothing => true
-        
-      rescue Exception => e
-        Goalog.critical_exception "Error processing PagSeguro Notification #{YAML::dump(@notification)}", e
-      end
-        
     else
       Goalog.debug "REDIRECT FROM PagSeguro: GET"
       respond_to do |format|
