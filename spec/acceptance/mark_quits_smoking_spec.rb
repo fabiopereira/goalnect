8446b8c29e23a4d7bfc090a3a4545250905556c8
Fabio Pereira <fabiopereira.me@gmail.com>
2012-11-08 16:34:15 -0300

    testing journey with mark quitting smoking and selecting a goal template

--- spec/acceptance/mark_quits_smoking_spec.rb
+++ spec/acceptance/mark_quits_smoking_spec.rb
@@ -7,7 +7,7 @@ feature 'Mark finds Goalnect and decides to quit smoking', %q{
   
   scenario 'Mark and his sudden decision to quit smoking' do  
     charity = ensure_charity_active_exists 'charitysmokingkills'
-    # goal = discover_goalnect_and_decide_to_commit_to_goal_template 'mark', 'Quit Smoking', charity
+    goal = discover_goalnect_and_decide_to_commit_to_goal_template 'mark', 'Quit Smoking', charity
   end
 
 end
