5e6f5df714fb5a35c9f99b7d0ffd03c797961614
Fabio Pereira <fabiopereira.me@gmail.com>
2012-12-02 11:51:46 -0300

    extracting method to show img in goal box

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -21,6 +21,17 @@ module ImageHelper
     image_tag(img_srcs[0], :class => "random_img_tag", "data-srcs" => img_srcs.join(","))
   end
 
+  def random_img_tag_for_goal goal, first_img = :goal_template
+    goal_template_img = img_src_full("goaltemplate", goal.goal_template, :thumb)
+    charity_img = img_src_full("charity", goal.charity, :thumb)
+    achiever_img = user_full_image_url(goal.achiever, :thumb)
+    if (first_img == :achiever)
+      random_img_tag [achiever_img, goal_template_img, charity_img]
+    else 
+      random_img_tag [goal_template_img, charity_img, achiever_img]
+    end 
+  end
+
   def img_src_full model_name, model, image_type
     model_has_image = model && model.image? && model.image_url(image_type)
     image_url = model_has_image ? model.image_url(image_type) : asset_path("nopic/#{model_name.downcase}-nopic-#{image_type}.png")
