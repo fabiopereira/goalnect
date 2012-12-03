ea0e90641d5da12ddb9b3b7eebf99fcc74a83668
Fabio Pereira <fabiopereira.me@gmail.com>
2012-12-03 15:17:40 -0300

    fixing locale per env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -150,7 +150,7 @@ class ApplicationController < ActionController::Base
   # http://guides.rubyonrails.org/i18n.html
   def set_locale
     I18n.locale = [
-        :pt, 
+        ENV['FIXED_LOCALE'], 
         set_locale_from_param,
         set_locale_from_current_user,
         set_locale_from_domain,
