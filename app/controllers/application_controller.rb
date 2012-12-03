ff553924c32eb2c6d633afb59355aa5993abd30d
Fabio Pereira <fabiopereira.me@gmail.com>
2012-12-03 03:04:11 -0300

    locale is always pt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -150,7 +150,7 @@ class ApplicationController < ActionController::Base
   # http://guides.rubyonrails.org/i18n.html
   def set_locale
     I18n.locale = [
-        # :en, 
+        :pt, 
         set_locale_from_param,
         set_locale_from_current_user,
         set_locale_from_domain,
