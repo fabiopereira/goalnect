a4fc8f880b0e251a0d6729323beb0d9ba44460c6
Fabio Pereira <fabiopereira.me@gmail.com>
2012-12-05 19:41:47 -0300

    posting goal image when posting to fb

--- app/assets/javascripts/goalnect_facebook.js
+++ app/assets/javascripts/goalnect_facebook.js
@@ -1,17 +1,17 @@
 function postToFeed(url, title, return_url, image_container) {
   // calling the API ...
-
-  // imgSrc = image_container.find("img").attr("src");
-  // alert(imgSrc);     
+  var host = $('<a>').prop('href', url).prop('hostname');
+  var imgSrc = host + $(image_container).find("img").attr("src");
 
   var obj = {
     method: 'feed',
     redirect_uri: return_url,
     link: url,
+    picture: imgSrc,
     name: title
   };
-  function callback(response) {
+  function callbackPostToFeed(response) {
     document.getElementById('msg').innerHTML = "Post ID: " + response['post_id'];
   }
-  FB.ui(obj, callback);
+  FB.ui(obj, callbackPostToFeed);
 }
