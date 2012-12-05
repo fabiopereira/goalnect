ddabe430bf05b292f31e55f99e5c487e96cdc8b9
Fabio Pereira <fabiopereira.me@gmail.com>
2012-12-05 15:15:56 -0300

    not logged in donation message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@ class HomeController < ApplicationController
   #before_filter :authenticate_user!
 
   def index
-    @goals = Goal.find(:all, :limit => 4, :order => "id desc")
+    @goals = Goal.find(:all, 
+            :limit => 4, 
+            :conditions => ['goal_stage_id not in (:inactive)', {:inactive => GoalStage.inactive_stages_id}],
+            :order => "id desc")
     respond_to do |format|
       format.html 
     end
