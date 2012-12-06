adeea9a38808141891527f71d42175bea28d9261
Fabio Pereira <fabiopereira.me@gmail.com>
2012-12-05 21:37:25 -0300

    fixing img for post to fb feed

--- app/assets/javascripts/goalnect_facebook.js
+++ app/assets/javascripts/goalnect_facebook.js
@@ -1,13 +1,18 @@
 function postToFeed(url, title, return_url, image_container) {
   // calling the API ...
-  var host = $('<a>').prop('href', url).prop('hostname');
-  var imgSrc = "http://" + host + $(image_container).find("img").attr("src");
+  var host = $('<a>').prop('href', url).prop('hostname');        
+  var imgSrc = $(image_container).find("img").attr("src")
+  
+  var imgPath = imgSrc;
+  if (imgPath.charAt(0) == "/") {
+	imgPath = "http://" + host + imgSrc;
+  }
 
   var obj = {
     method: 'feed',
     redirect_uri: return_url,
     link: url,
-    picture: imgSrc,
+    picture: imgPath,
     name: title
   };
   function callbackPostToFeed(response) {
