4bc961be5054032bf2da7b21a1b6930894755683
Fabio Pereira <fabiopereira.me@gmail.com>
2012-12-07 17:34:54 -0300

    creating goals explore page

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,7 +1,16 @@
 class ExploreController < ApplicationController
   include GoalExplorer
   
+  def goals_from_donations goal_donations
+    goal_donations.map {|d| d.goal }
+  end
+  
   def goals
-    
+    @goals_latest = Goal.latest.explore_limit
+
+    recent_donations = GoalDonation.approved.recent.includes(:goal).limit(3)
+    @goals_with_recent_donation = goals_from_donations(recent_donations)
+
+    @goals_random_scope = Goal.random_scope.explore_limit
   end
 end
