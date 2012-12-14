acecadb238383b1b5c577b175b551a8b3b1b0411
Camila Hayashi <camilahayashi@gmail.com>
2012-12-14 00:38:51 -0300

    changed layout page show events

--- app/helpers/goal_template_helper.rb
+++ app/helpers/goal_template_helper.rb
@@ -8,5 +8,9 @@ module GoalTemplateHelper
    "/goals/new?goal_template=#{event_title}"
   end
   
+  def title_facebook_for_event event
+    t('events.post_on_facebook_title', 
+      :event_title => event.title)
+  end
   
 end
